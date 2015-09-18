1a6bb6edc0c279647f06cc8c2fd131029d7f0984
Thomas  Brawner <thomasbrawner@gmail.com>
2015-09-18 17:01:55 -0600

    add within topic exploration

--- topics.py
+++ topics.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd 
 import seaborn as sns 
+from collections import defaultdict
 from nltk.corpus import stopwords
 from nmf import NMFactor 
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from wordcloud import WordCloud
 
 ## ----------------------------------------------------- ##
@@ -34,14 +35,140 @@ feature_names = tfidf.get_feature_names()
 ## --------------------------------------------------------------------- ##
 ## topics
 
-nmf = NMFactor(k = 10)
+nmf = NMFactor(k = 20)
 nmf.solve(V = features, feature_names = feature_names, dates = data['date'], labels = data['source']) 
+data['topic'] = nmf.topic_preds 
 
+## --------------------------------------------------------------------- ##
+## within topics 
+
+def scale_conversion(df, new_scale):
+	'''
+	convert values of df from arbitrary range to (min, max), then 
+	round the result to integer
+	'''
+	old_range = (df.max().max() - df.min().min())  
+	new_range = (new_scale[1] - new_scale[0])  
+	new_df = (((df - df.min().min()) * new_range) / old_range) + 1
+	return np.round(new_df)
+
+
+def n_largest(arr, n): return np.argpartition(arr, -n)[-n:]
+
+def cloud_word_string(tfidf_mat, tfidf_names, count_mat, count_names):
+
+	tfidf_data = {}
+	for source in tfidf_mat.index.values:
+		row = tfidf_mat.ix[source]
+		term_idx = n_largest(row, 25)
+		terms = np.array(tfidf_names)[term_idx]
+		values = np.array(row)[term_idx]
+		tfidf_data[source] = ' '.join([' '.join([x] * y) for x, y in zip(terms, values)])
+
+	count_data = {}
+	for source in count_mat.index.values:
+		row = count_mat.ix[source]
+		term_idx = n_largest(row, 25)
+		terms = np.array(count_names)[term_idx]
+		values = np.array(row)[term_idx]
+		count_data[source] = ' '.join([' '.join([x] * y) for x, y in zip(terms, values)])
+
+	return {key : tfidf_data[key] + count_data[key] for key in tfidf_data}
+
+def within_label_cloud_by_topic(data_dict, topic):
+		'''
+		Explore the topics within the labels. 
+		'''
+		
+		try:
+			guardian = WordCloud(background_color = 'white', width = 800, height = 1800).generate(data_dict['Guardian'])
+			nyt = WordCloud(background_color = 'white', width = 800, height = 1800).generate(data_dict['New York Times'])
+			wsj = WordCloud(background_color = 'white', width = 800, height = 1800).generate(data_dict['Wall Street Journal'])
+
+			plt.figure(figsize = (9, 6))
+			ax1 = plt.subplot2grid((5, 9), (0, 0), rowspan = 5, colspan = 3)
+			ax2 = plt.subplot2grid((5, 9), (0, 3), rowspan = 5, colspan = 3)
+			ax3 = plt.subplot2grid((5, 9), (0, 6), rowspan = 5, colspan = 3)
+
+			ax1.imshow(guardian)
+			ax1.axis('off')
+			ax1.set_title('Guardian', y = 1.02, fontsize = 18)
+
+			ax2.imshow(nyt)
+			ax2.axis('off')
+			ax2.set_title('New York Times', y = 1.02, fontsize = 18)		
+
+			ax3.imshow(wsj)
+			ax3.axis('off')
+			ax3.set_title('Wall Street Journal', y = 1.02, fontsize = 18)		
+
+			plt.tight_layout() 
+			plt.savefig('figures/source_within_topic' + str(topic) + '.png') 
+			plt.close() 
+			return 
+		except:
+			pass 
+
+## --------------------------------------------------------------------- ##
+## 
+"""
+tfidf = TfidfVectorizer(stop_words = augmented_stop_words, max_features = 500)
+count = CountVectorizer(stop_words = augmented_stop_words, max_features = 500)
+
+zero = data.query('topic == 0')
+
+tfidf_features = tfidf.fit_transform(zero['text'])
+tfidf_names = tfidf.get_feature_names()
+
+tfidf_terms = pd.DataFrame(tfidf_features.toarray(), index = zero['source'])
+tfidf_terms = tfidf_terms.groupby(tfidf_terms.index.values).mean() 
+
+count_features = count.fit_transform(zero['text'])
+count_names = count.get_feature_names() 
+
+count_terms = pd.DataFrame(count_features.toarray(), index = zero['source'])
+count_terms = count_terms.groupby(count_terms.index.values).mean() 
+
+t_convert = scale_conversion(tfidf_terms, (1, 1000))
+c_convert = scale_conversion(count_terms, (1, 1000))
+
+check = cloud_word_string(t_convert, tfidf_names, c_convert, count_names)
+within_label_cloud_by_topic(check)
+
+import sys; sys.exit() 
+"""
 ## --------------------------------------------------------------------- ##
 ## plot topics by source 
 
+tfidf = TfidfVectorizer(stop_words = augmented_stop_words, max_features = 500)
+count = CountVectorizer(stop_words = augmented_stop_words, max_features = 500)
+
 for i in xrange(nmf.k):
+
+	# topics 
 	nmf.plot_topic(i)
+
+	# look within topics 
+	topic_data = data.query('topic == {0}'.format(str(i)))
+
+	tfidf_features = tfidf.fit_transform(topic_data['text'])
+	tfidf_names = tfidf.get_feature_names()
+
+	tfidf_terms = pd.DataFrame(tfidf_features.toarray(), index = topic_data['source'])
+	tfidf_terms = tfidf_terms.groupby(tfidf_terms.index.values).mean() 
+
+	count_features = count.fit_transform(topic_data['text'])
+	count_names = count.get_feature_names() 
+
+	count_terms = pd.DataFrame(count_features.toarray(), index = topic_data['source'])
+	count_terms = count_terms.groupby(count_terms.index.values).mean() 
+
+	t_convert = scale_conversion(tfidf_terms, (1, 1000))
+	c_convert = scale_conversion(count_terms, (1, 1000))
+
+	string_dict = cloud_word_string(t_convert, tfidf_names, c_convert, count_names)
+	within_label_cloud_by_topic(string_dict, i)
+
 
 ## --------------------------------------------------------------------- ##
 ## --------------------------------------------------------------------- ##
